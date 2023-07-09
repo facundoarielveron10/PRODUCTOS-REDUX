a62247a6642c59fb29b5cf995c9683a515ad911b
facundoarielveron10 <facundoarielveron10@gmail.com>
2023-07-09 20:50:15 -0300

    Avanzando en el proyecto

--- src/actions/ProductoActions.js
+++ src/actions/ProductoActions.js
@@ -2,6 +2,9 @@ import {
 	AGREGAR_PRODUCTO,
 	AGREGAR_PRODUCTO_EXITO,
 	AGREGAR_PRODUCTO_ERROR,
+	COMENZAR_DESCARGA_PRODUCTOS,
+	DESCARGA_PRODUCTOS_EXITO,
+	DESCARGA_PRODUCTOS_ERROR,
 } from '../types';
 import clienteAxios from '../config/axios';
 import Swal from 'sweetalert2';
@@ -36,18 +39,43 @@ export function crearNuevoProductoAction(producto) {
 		}
 	};
 }
-
 const agregarProducto = () => ({
 	type: AGREGAR_PRODUCTO,
 	payload: true,
 });
-
 const agregarProductoExito = producto => ({
 	type: AGREGAR_PRODUCTO_EXITO,
 	payload: producto,
 });
-
 const agregarProductoError = estado => ({
 	type: AGREGAR_PRODUCTO_ERROR,
+	payload: estado,
+});
+
+// Descargar productos
+export function obtenerProductosAction() {
+	return async dispatch => {
+		// Intentamos obtener los productos
+		dispatch(descargarProductos());
+		try {
+			// Obtener los productos de la API
+			const { data } = await clienteAxios.get('/productos');
+			dispatch(descargarProductosExito(data));
+		} catch (error) {
+			console.log(error);
+			dispatch(descargarProductosError(true));
+		}
+	};
+}
+const descargarProductos = () => ({
+	type: COMENZAR_DESCARGA_PRODUCTOS,
+	payload: true,
+});
+const descargarProductosExito = productos => ({
+	type: DESCARGA_PRODUCTOS_EXITO,
+	payload: productos,
+});
+const descargarProductosError = estado => ({
+	type: DESCARGA_PRODUCTOS_ERROR,
 	payload: estado,
 });
--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -1,6 +1,18 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
+
+// Redux
+import { useDispatch, useSelector } from 'react-redux';
+import { obtenerProductosAction } from '../actions/ProductoActions';
 
 export default function Productos() {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		// Consultar la API
+		const cargarProductos = () => dispatch(obtenerProductosAction());
+		cargarProductos();
+	}, [dispatch]);
+
 	return (
 		<Fragment>
 			<h2 className="text-center my-5">Listado de Productos</h2>
